8d7ddc5604b4a1960cfb524783cb75f616ef0271
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-16 03:36:06 +0900

    Updates

---
+++ precache-manifest.63f668743085eddfd541f765a27d3f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfd8f0da1251616360b11b66e9cd5ff",
+    "url": "%7Busername%7D.github.io/%7Brepository-name%7D/index.html"
+  },
+  {
+    "revision": "0f96d81e93241027b63a",
+    "url": "%7Busername%7D.github.io/%7Brepository-name%7D/static/css/main.ba973ddc.chunk.css"
+  },
+  {
+    "revision": "0d15cc4c1d32f51c6983",
+    "url": "%7Busername%7D.github.io/%7Brepository-name%7D/static/js/2.7d852036.chunk.js"
+  },
+  {
+    "revision": "0f96d81e93241027b63a",
+    "url": "%7Busername%7D.github.io/%7Brepository-name%7D/static/js/main.f32862bd.chunk.js"
+  },
+  {
+    "revision": "8f472449bbcecb195c42",
+    "url": "%7Busername%7D.github.io/%7Brepository-name%7D/static/js/runtime~main.26eefc0e.js"
+  }
+]);
