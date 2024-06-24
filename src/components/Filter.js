1673b0e5193e9a5bdc1da770f02ec90986262bd1
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-25 03:38:21 +0900

    design8 commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -38,31 +38,33 @@ const Filter = ({ setLocationFilter, setDateFilter, setGenderFilter, setTypeFilt
     };
 
     return (
-        <div className="filter">
-            <label>
-                위치 필터:
-                <input type="text" value={location} onChange={handleLocationChange} />
-            </label>
-            <label>
-                유실 날짜 필터:
-                <input type="text" placeholder="yyyy, yyyy-mm, yyyy-mm-dd" value={date} onChange={handleDateChange} />
-            </label>
-            <label>
-                성별 필터:
-                <select value={gender} onChange={handleGenderChange}>
-                    <option value="">All</option>
-                    <option value="암">암</option>
-                    <option value="수">수</option>
-                </select>
-            </label>
-            <label>
-                종류 필터:
-                <select value={type} onChange={handleTypeChange}>
-                    <option value="">All</option>
-                    <option value="개">개</option>
-                    <option value="고양이">고양이</option>
-                </select>
-            </label>
+        <div className="filter-container">
+            <div className="filter">
+                <label>
+                    위치 필터:
+                    <input type="text" value={location} onChange={handleLocationChange} />
+                </label>
+                <label>
+                    유실 날짜 필터:
+                    <input type="text" placeholder="yyyy, yyyy-mm, yyyy-mm-dd" value={date} onChange={handleDateChange} />
+                </label>
+                <label>
+                    성별 필터:
+                    <select value={gender} onChange={handleGenderChange}>
+                        <option value="">All</option>
+                        <option value="암">암</option>
+                        <option value="수">수</option>
+                    </select>
+                </label>
+                <label>
+                    종류 필터:
+                    <select value={type} onChange={handleTypeChange}>
+                        <option value="">All</option>
+                        <option value="개">개</option>
+                        <option value="고양이">고양이</option>
+                    </select>
+                </label>
+            </div>
             <div className="filter-buttons">
                 <button onClick={resetFilters}>필터 초기화</button>
                 <button>필터 적용</button>
