d1731df4a9466edb2dca61a981d6dae21c60b5ae
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-25 03:09:16 +0900

    design5 commit

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -7,20 +7,20 @@ const DogCard = ({ dog }) => {
         : `${process.env.PUBLIC_URL}/images/default_photo.jpg`;
 
     return (
-        <div className="dog-card border p-4 rounded shadow-lg relative" style={{position: 'relative' }}>
+        <div className="dog-card border p-4 rounded shadow-lg relative" style={{ position: 'relative' }}>
             <img src={imageUrl} alt={dog['이름']} className="w-full h-48 object-cover rounded mb-4" />
             <h2>{dog['이름']}</h2>
-            <p><strong>유실 날짜:</strong> {dog['유실 날짜']}</p>
-            <p><strong>성별:</strong> {dog['성별']}</p>
-            <p><strong>견종/묘종:</strong> {dog['견종/묘종']}</p>
-            <p><strong>유실 장소:</strong> {dog['시, 군, 구']}, {dog['동, 읍, 면']}, {dog['세부 주소']}</p>
+            <p className="info"><strong>유실 날짜:</strong> {dog['유실 날짜']}</p>
+            <p className="info"><strong>성별:</strong> {dog['성별']}</p>
+            <p className="info"><strong>견종/묘종:</strong> {dog['견종/묘종']}</p>
+            <p className="info"><strong>유실 장소:</strong> {dog['시, 군, 구']}, {dog['동, 읍, 면']}, {dog['세부 주소']}</p>
             {dog['URL'] && (
-                <p>
-                 <strong>카톡 오픈채팅:</strong>
-                 <a href={dog['URL']} target="_blank" rel="noopener noreferrer" style={{ display: 'inline-block', wordWrap: 'break-word' }}>{dog['URL']}</a>
-                 </p>
+                <p className="info">
+                    <strong>카톡 오픈채팅:</strong>
+                    <a href={dog['URL']} target="_blank" rel="noopener noreferrer" style={{ display: 'inline-block', wordWrap: 'break-word' }}>{dog['URL']}</a>
+                </p>
             )}
-            <Link to={`/dog-details/${dog['이름']}`} className="more-details" style={{ position: 'absolute', bottom: '20px', right: '20px', color: '#007bff', textDecoration: 'none' }}>더 보기 ...</Link>
+            <Link to={`/dog-details/${dog['이름']}`} className="more-details">더 보기 ...</Link>
         </div>
     );
 };
