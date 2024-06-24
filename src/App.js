e58f2b7559430ff1e80314ab47d92f310e520612
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-25 04:46:58 +0900

    design13 commit

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ const App = () => {
                     <h1><Link to="/" onClick={() => window.location.reload()}>지해피독 유실 동물 리스트</Link></h1>
                     <p>지해피독에 오신 것을 환영합니다. 여기서 유실된 강아지 정보를 찾고 주인과 다시 만날 수 있도록 도와주세요.</p>
                     <nav className="nav-list">
-                        <Link to="/ghappydog/lost">유실동물</Link>
-                        <Link to="/ghappydog/found">발견 동물</Link>
+                        <Link to="/lost">유실동물</Link>
+                        <Link to="/found">발견 동물</Link>
                     </nav>
                 </header>
                 <Routes>
-                    <Route path="/ghappydog/lost" element={<LostAnimals lostAnimals={dogs} />} />
-                    <Route path="/ghappydog/found" element={<FoundAnimals foundAnimals={foundDogs} />} />
-                    <Route path="/ghappydog/dog-details/:name" element={<DogDetails animals={dogs.concat(foundDogs)} />} />
+                    <Route path="/lost" element={<LostAnimals lostAnimals={dogs} />} />
+                    <Route path="/found" element={<FoundAnimals foundAnimals={foundDogs} />} />
+                    <Route path="/dog-details/:name" element={<DogDetails animals={dogs.concat(foundDogs)} />} />
                 </Routes>
             </div>
         </Router>
