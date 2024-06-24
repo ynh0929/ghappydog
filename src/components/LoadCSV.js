cc88390fa6a626b585c1bb4a5ccdbb60f00536fc
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-24 23:41:14 +0900

    tailwind6 commit

--- src/components/LoadCSV.js
+++ src/components/LoadCSV.js
@@ -3,31 +3,23 @@ import Papa from 'papaparse';
 
 const LoadCSV = ({ setLostAnimals, setFoundAnimals }) => {
     useEffect(() => {
-        fetch('/지해피독 수색용 유실견 리스트.csv')
-            .then(response => response.text())
-            .then(csvText => {
-                Papa.parse(csvText, {
-                    header: true,
-                    dynamicTyping: true,
-                    complete: (result) => {
-                        setLostAnimals(result.data);
-                    },
-                });
-            })
-            .catch(error => console.error('Error fetching lost animals CSV:', error));
+        const loadCSV = (url, setData) => {
+            fetch(url)
+                .then(response => response.text())
+                .then(csvText => {
+                    Papa.parse(csvText, {
+                        header: true,
+                        dynamicTyping: true,
+                        complete: (result) => {
+                            setData(result.data);
+                        },
+                    });
+                })
+                .catch(error => console.error(`Error fetching ${url}:`, error));
+        };
 
-        fetch('/지해피독_찾음.csv')
-            .then(response => response.text())
-            .then(csvText => {
-                Papa.parse(csvText, {
-                    header: true,
-                    dynamicTyping: true,
-                    complete: (result) => {
-                        setFoundAnimals(result.data);
-                    },
-                });
-            })
-            .catch(error => console.error('Error fetching found animals CSV:', error));
+        loadCSV(`${process.env.PUBLIC_URL}/지해피독 수색용 유실견 리스트.csv`, setLostAnimals);
+        loadCSV(`${process.env.PUBLIC_URL}/지해피독_찾음.csv`, setFoundAnimals);
     }, [setLostAnimals, setFoundAnimals]);
 
     return null;
