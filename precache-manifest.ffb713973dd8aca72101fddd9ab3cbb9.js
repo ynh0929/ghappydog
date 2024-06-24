cb8ba6fd5c930c2012d4f7489ab8788c82a50173
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-24 23:06:17 +0900

    Updates

--- precache-manifest.ffb713973dd8aca72101fddd9ab3cbb9.js
+++ precache-manifest.ffb713973dd8aca72101fddd9ab3cbb9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0289db181988af734cf6b38fb905e7c",
+    "revision": "0f208de1e6b40c1eea0a567d9471c8a5",
     "url": "/ghappydog/index.html"
   },
   {
-    "revision": "47bd33b5e148b8b73e40",
+    "revision": "374a57e16711e7e19df0",
     "url": "/ghappydog/static/css/main.9114b1ea.chunk.css"
   },
   {
-    "revision": "23a51e0fd38a5954f0cd",
-    "url": "/ghappydog/static/js/2.a17811bf.chunk.js"
+    "revision": "0d15cc4c1d32f51c6983",
+    "url": "/ghappydog/static/js/2.7d852036.chunk.js"
   },
   {
-    "revision": "47bd33b5e148b8b73e40",
-    "url": "/ghappydog/static/js/main.d25bcad9.chunk.js"
+    "revision": "374a57e16711e7e19df0",
+    "url": "/ghappydog/static/js/main.8ebc6449.chunk.js"
   },
   {
     "revision": "8367f84abe28b1a5af96",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ghappydog/precache-manifest.c276d766f85a1a053d9c6f8121537a03.js"
+  "/ghappydog/precache-manifest.ffb713973dd8aca72101fddd9ab3cbb9.js"
 );
 
 self.addEventListener('message', (event) => {
