4db72f249ce1f5fbcbcb64ddbdac9588f0ac7bcf
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-06-25 02:07:11 +0900

    design2 commit

--- src/components/DogDetails.js
+++ src/components/DogDetails.js
@@ -15,19 +15,23 @@ const DogDetails = ({ animals }) => {
 
     return (
         <div className="dog-details container mx-auto p-4">
-            <img src={imageUrl} alt={dog['이름']} className="dog-details-img" />
-            <h2>{dog['이름']}</h2>
-            <p><strong>유실 날짜:</strong> {dog['유실 날짜']}</p>
-            <p><strong>성별:</strong> {dog['성별']}</p>
-            <p><strong>견종/묘종:</strong> {dog['견종/묘종']}</p>
-            <p><strong>색상:</strong> {dog['색상']}</p>
-            <p><strong>몸무게:</strong> {dog['몸무게']}</p>
-            <p><strong>유실 장소:</strong> {`${dog['시, 군, 구']}, ${dog['동, 읍, 면']}, ${dog['세부 주소']}`}</p>
-            <p><strong>세부 특징:</strong> {dog['그 외 세부 특징']}</p>
-            <p><strong>마지막 목격 지역:</strong> {dog['마지막 제보 지역']}</p>
-            <p><strong>인식칩:</strong> {dog['인식칩']}</p>
-            <strong>카톡 오픈채팅:</strong> 
-            <a href={dog['URL']} target="_blank" rel="noopener noreferrer" style={{ marginLeft: '5px' }}>{dog['URL']}</a>
+            <div className="dog-details-content">
+                <div className="dog-details-info">
+                    <h2>{dog['이름']}</h2>
+                    <p><strong>유실 날짜:</strong> {dog['유실 날짜']}</p>
+                    <p><strong>성별:</strong> {dog['성별']}</p>
+                    <p><strong>견종/묘종:</strong> {dog['견종/묘종']}</p>
+                    <p><strong>색상:</strong> {dog['색상']}</p>
+                    <p><strong>몸무게:</strong> {dog['몸무게']}</p>
+                    <p><strong>유실 장소:</strong> {`${dog['시, 군, 구']}, ${dog['동, 읍, 면']}, ${dog['세부 주소']}`}</p>
+                    <p><strong>세부 특징:</strong> {dog['그 외 세부 특징']}</p>
+                    <p><strong>마지막 목격 지역:</strong> {dog['마지막 제보 지역']}</p>
+                    <p><strong>인식칩:</strong> {dog['인식칩']}</p>
+                </div>
+                <div className="dog-details-image">
+                    <img src={imageUrl} alt={dog['이름']} className="dog-details-img" />
+                </div>
+            </div>
         </div>
     );
 };
