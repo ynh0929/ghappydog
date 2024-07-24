b2322bd188f91294b849149cdfcdfda69e84d2e2
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-07-25 07:26:47 +0900

    13.2 commit

--- src/components/DogDetails.js
+++ src/components/DogDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './DogDetails.css'; // Create and import a CSS file for styling
 
 const DogDetails = ({ animals }) => {
     const { name } = useParams();
@@ -13,7 +14,6 @@ const DogDetails = ({ animals }) => {
         ? `${process.env.PUBLIC_URL}/images/${dog['이미지'].replace(/\.([a-zA-Z]+)$/, match => match.toLowerCase())}`
         : `${process.env.PUBLIC_URL}/images/default_photo.jpg`;
 
-    // Assuming you have additional image URLs in the data
     const additionalImages = [
         dog['추가 이미지1'],
         dog['추가 이미지2'],
@@ -23,28 +23,61 @@ const DogDetails = ({ animals }) => {
 
     return (
         <div className="dog-details container mx-auto p-4">
-            <div className="dog-details-content">
-                <div className="dog-details-info">
-                    <h2>{dog['이름']}</h2>
-                    <p><strong>유실 날짜:</strong> {dog['유실 날짜']}</p>
-                    <p><strong>성별:</strong> {dog['성별']}</p>
-                    <p><strong>견종/묘종:</strong> {dog['견종/묘종']}</p>
-                    <p><strong>색상:</strong> {dog['색상']}</p>
-                    <p><strong>몸무게:</strong> {dog['몸무게']}</p>
-                    <p><strong>유실 장소:</strong> {`${dog['도']} ${dog['시, 군, 구']} ${dog['동, 읍, 면']} ${dog['세부 주소']}`}</p>
-                    <p><strong>세부 특징:</strong> {dog['그 외 세부 특징']}</p>
-                    <p><strong>마지막 목격 지역:</strong> {dog['마지막 제보 지역']}</p>
-                    <p><strong>인식칩:</strong> {dog['인식칩']}</p>
-                </div>
-                <div className="dog-details-image">
-                    <img src={imageUrl} alt={dog['이름']} className="dog-details-img" />
-                </div>
+            <div className="dog-details-header">
+                <h2>{dog['이름']}</h2>
             </div>
-            <p><strong>상세 이미지: </strong></p><br />
-            <div className="additional-images mt-4">
-                {additionalImages.map((img, index) => (
-                    <img key={index} src={img} alt={`${dog['이름']} 추가 이미지 ${index + 1}`} className="additional-image rounded shadow-lg" />
-                ))}
+            <div className="dog-details-content">
+                <table className="dog-details-table">
+                    <tbody>
+                        <tr>
+                            <td><strong>유실 날짜:</strong></td>
+                            <td>{dog['유실 날짜']}</td>
+                            <td rowSpan="8" className="dog-details-image">
+                                <img src={imageUrl} alt={dog['이름']} className="dog-details-img" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>성별:</strong></td>
+                            <td>{dog['성별']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>견종/묘종:</strong></td>
+                            <td>{dog['견종/묘종']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>색상:</strong></td>
+                            <td>{dog['색상']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>몸무게:</strong></td>
+                            <td>{dog['몸무게']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>유실 장소:</strong></td>
+                            <td>{`${dog['도']} ${dog['시, 군, 구']} ${dog['동, 읍, 면']} ${dog['세부 주소']}`}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>세부 특징:</strong></td>
+                            <td>{dog['그 외 세부 특징']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>마지막 목격 지역:</strong></td>
+                            <td>{dog['마지막 제보 지역']}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>인식칩:</strong></td>
+                            <td>{dog['인식칩']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="additional-images-section">
+                <p><strong>상세 이미지: </strong></p><br />
+                <div className="additional-images mt-4">
+                    {additionalImages.map((img, index) => (
+                        <img key={index} src={img} alt={`${dog['이름']} 추가 이미지 ${index + 1}`} className="additional-image rounded shadow-lg" />
+                    ))}
+                </div>
             </div>
         </div>
     );
