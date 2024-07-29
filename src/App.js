b28f89422d17bed2200183c8f137ae0ad7ee7ad5
Ye Na Hwang <yhwang20@my.bcit.ca>
2024-07-29 20:07:53 +0900

    07.29.3 commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import FoundAnimals from './components/FoundAnimals';
 import './App.css'; // Ensure this import is present if you have App-specific styles
 import './index.css'; // Ensure this import is present
 
+
+// Import the logo image
+import logo from '../public/logo.png';
+
 const App = () => {
     const [dogs, setDogs] = useState([]);
     const [foundDogs, setFoundDogs] = useState([]);
@@ -26,6 +30,7 @@ const App = () => {
             <LoadCSV setLostAnimals={setDogs} setFoundAnimals={setFoundDogs} />
             <div className="container">
                 <header>
+                    <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
                     <h1><Link to="/" onClick={() => window.location.reload()}>지해피독 유실 동물 리스트</Link></h1>
                     <p>지해피독에 오신 것을 환영합니다. 여기서 유실된 강아지 정보를 찾고 주인과 다시 만날 수 있도록 도와주세요.</p>
                     <nav className="nav-list">
